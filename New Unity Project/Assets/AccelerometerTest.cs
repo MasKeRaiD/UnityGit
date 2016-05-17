c4361678a1ae52c40e53b8ad54deb157b1ce242a
MasKeRaiD <mk10@hotmail.de>
2016-05-17 17:37:48 +0200

    1.5
    pinching läuft jetzt und accelerometer funktioniert mehr oder weniger.
    brauchen aber color limits, damit der cube nicht schwarz wird.

---
+++ New Unity Project/Assets/AccelerometerTest.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelerometerTest : MonoBehaviour {
+    //RGB
+    float red = 0f;
+    float blue = 0f;
+    float green = 0f;
+	public float speed = 3f;
+	public Color altColor = Color.white;
+	public Renderer rend;
+
+
+	void Example() {
+		altColor.g = 1f;
+		altColor.r = 0f;
+		altColor.b = 0f;
+		altColor.a = 0f;
+	}
+    // Use this for initialization
+    void Start () {
+		Example ();
+		gameObject.GetComponent<Renderer> ().material.color = altColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Close on 'back'
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+        //do something
+
+
+        //red
+        if (Input.acceleration.x > 0)
+        {
+				altColor.r += 1f;
+			gameObject.GetComponent<Renderer> ().material.color = altColor;
+        }
+
+        if (Input.acceleration.x < 0)
+        {
+			altColor.r -= 1f;
+			gameObject.GetComponent<Renderer> ().material.color = altColor;
+        }
+
+       
+        //blue
+        if (Input.acceleration.y > 0)
+        {
+			altColor.b += 1f;
+			gameObject.GetComponent<Renderer> ().material.color = altColor;
+        }
+
+        if (Input.acceleration.y < 0)
+        {
+			altColor.b -= 1f;
+			gameObject.GetComponent<Renderer> ().material.color = altColor;
+		}
+       
+       
+        //green
+		if (Input.acceleration.z > 0)
+        {
+			altColor.g += 1f;
+			gameObject.GetComponent<Renderer> ().material.color = altColor;
+        }
+
+        if (Input.acceleration.z < 0)
+        {
+			altColor.g -= 1f;
+			gameObject.GetComponent<Renderer> ().material.color = altColor;
+        }
+        
+
+    }
+}
+
+
---
+++ New Unity Project/Assets/TouchRotation.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchRotation : MonoBehaviour
+{
+
+    GameObject targetItem;
+    Camera GUICamera;
+    GameObject ambient;
+
+
+    /********Rotation Variables*********/
+    float rotationRate = 1.0f;
+    bool wasRotating;
+
+    /************Scrolling inertia variables************/
+    private Vector2 scrollPosition = Vector2.zero;
+    private float scrollVelocity = 0;
+    private float timeTouchPhaseEnded;
+    private float inertiaDuration = 0.5f;
+
+    private float itemInertiaDuration = 1.0f;
+    private float itemTimeTouchPhaseEnded;
+    private float rotateVelocityX = 0;
+    private float rotateVelocityY = 0;
+
+
+    RaycastHit hit;
+
+    //private int layerMask = (1 << 8) | (1 << 2);
+    //private var layerMask = (1 <<  0);
+
+    void Start()
+    {
+        //layerMask = ~layerMask;
+    }
+
+    void FixedUpdate()
+    {
+
+        if (Input.touchCount > 0)
+        {        //    If there are touches...
+            var theTouch = Input.GetTouch(0);        //    Cache Touch (0)
+
+            var ray = Camera.main.ScreenPointToRay(theTouch.position);
+            //Ray GUIRayq = GUICamera.ScreenPointToRay(theTouch.position);
+
+
+           //if (Physics.Raycast(ray, out hit, 50f, layerMask))
+           //{
+
+                if (Input.touchCount == 1)
+                {
+
+                    if (theTouch.phase == TouchPhase.Began)
+                    {
+                        wasRotating = false;
+                    }
+
+                    if (theTouch.phase == TouchPhase.Moved)
+                    {
+
+                        gameObject.transform.Rotate(theTouch.deltaPosition.y * rotationRate, -theTouch.deltaPosition.x * rotationRate, 0, Space.World);
+                        wasRotating = true;
+                    }
+
+                }
+            //}
+        }
+        
+    }
+}
